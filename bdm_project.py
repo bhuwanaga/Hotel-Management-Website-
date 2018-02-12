29e81dcb9a27a6489cc7d9259a2a6ab0e1c570ec
bhuwanaga <32989142+bhuwanaga@users.noreply.github.com>
2018-02-11 22:02:36 -0500

    Update bdm_project.py

--- bdm_project.py
+++ bdm_project.py
@@ -18,9 +18,9 @@ print "}</style>"
 print ""
 
 db = MySQLdb.connect(host="localhost",  # Hostname
-                     user="fancy",  # Username
-                     passwd="mysql",  # Password
-                     db="hotel")  # Schema
+                     user="*****",  # Username
+                     passwd="******",  # Password
+                     db="******")  # Schema
 
 print '<form method="post" action="bdm_project.py">'
 print "	Name:<br>"
